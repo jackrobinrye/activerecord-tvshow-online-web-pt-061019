4badcb8d7e49625b259532e118d074dbe39cb4c5
Jack Atherton <jackrrye@gmail.com>
2019-08-30 19:31:47 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -2,7 +2,7 @@
 # name, network, and day have a datatype of string, and
 # rating has a datatype of integer.
 
-class CreateShows < ActiveRecord::Migrate[5.2]
+class CreateShows < ActiveRecord::Migration[5.2]
 
 def change
   create_table(:shows) do |t|
