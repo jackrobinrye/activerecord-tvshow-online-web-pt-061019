ca12da7458c331aa9f45e3d25a09477c769d2f3e
Jack Atherton <jackrrye@gmail.com>
2019-08-30 19:24:54 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,17 +1,17 @@
-# name, network, day, and rating columns. 
-# name, network, and day have a datatype of string, and 
+# name, network, day, and rating columns.
+# name, network, and day have a datatype of string, and
 # rating has a datatype of integer.
 
 class CreateShows
 
 def change
   create_table(:shows) do |t|
-    t.string :name 
-    t.string :network 
-    t.string :day 
-    t.integer :rating 
-  end 
-end 
+    t.string :name
+    t.string :network
+    t.string :day
+    t.integer :rating
+  end
+end
 
 
 
